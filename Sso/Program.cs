7520f631b69b939660bc35f62a14bea56cfec86d
Alex Haslehurst <alex.haslehurst@gmail.com>
2024-07-18 20:14:33 +0100

    Initial commit

---
+++ Sso/Program.cs
@@ -0,0 +1,53 @@
+﻿using Sso;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((ctx, lc) => lc
+    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+    .Enrich.FromLogContext()
+    .ReadFrom.Configuration(ctx.Configuration));
+
+builder.Services.AddRazorPages();
+
+var isBuilder = builder.Services.AddIdentityServer(options =>
+    {
+        // TODO disable on ssl
+        options.Authentication.CookieSameSiteMode = SameSiteMode.Lax;
+                
+        options.Events.RaiseErrorEvents = true;
+        options.Events.RaiseInformationEvents = true;
+        options.Events.RaiseFailureEvents = true;
+        options.Events.RaiseSuccessEvents = true;
+
+        // see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
+        options.EmitStaticAudienceClaim = true;
+    })
+    .AddTestUsers(TestUsers.Users);
+
+// in-memory, code config
+isBuilder.AddInMemoryIdentityResources(Config.IdentityResources);
+isBuilder.AddInMemoryApiScopes(Config.ApiScopes);
+isBuilder.AddInMemoryClients(Config.Clients);
+
+builder.Services.AddAuthentication();
+
+
+var app = builder.Build();
+
+app.UseSerilogRequestLogging();
+    
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseIdentityServer();
+app.UseAuthorization();
+        
+app.MapRazorPages()
+    .RequireAuthorization();
+    
+app.Run();
